40dcda2359477a462db536fb78e0a25e1a7697a3
Duke Embezla <dukembezla@yahoo.com>
2014-12-15 11:54:29 -0500

    Fixed update student

--- src/main/webapp/resources/js/main.js
+++ src/main/webapp/resources/js/main.js
@@ -1,3 +1,15 @@
+// used from http://www.w3schools.com/js/js_cookies.asp
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0; i<ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1);
+        if (c.indexOf(name) != -1) return c.substring(name.length, c.length);
+    }
+    return "";
+}
+
 $(document).ready(function() {
 
     // Activated when clicking "Home" from left navigation panel
@@ -32,10 +44,13 @@ $(document).ready(function() {
         } else if (!($("#email").val()).match(email)) {
             alert("Invalid email entered.");
         } else {
+            // get user student ID from session cookie
+            var studentId = getCookie("studentId");
             $.ajax({
                 url: "updateStudent",
                 contentType: "application/json",
                 data: JSON.stringify({
+                    "id": studentId,
                     "name": $("#name").val(),
                     "email": $("#email").val(),
                     "discipline": $("#degree").val()
